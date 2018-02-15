d2b58db0418dbfcde4515140e64f39e6e5793e9f
adrianpothuaud <adrianpothuaud2@gmail.com>
2018-02-15 07:58:36 +0100

    begin scaffolding

--- allos/generate.sikuli/generate.py
+++ allos/generate.sikuli/generate.py
@@ -10,15 +10,27 @@ def create_sikuli_script(path, content=None):
     '''create a complete sikuli script'''
     print('creation of a sikuli script at {}'.format(path))
     if not '.sikuli' in path:
-        path.append('.sikuli')
+        path = u'{}.sikuli'.format(path)
     name = path.split('.')[-2].split(os.sep)[-1]
     os.system('mkdir {}'.format(path))
     os.system('touch {}{}{}.py'.format(path, os.sep, name))
     os.system('touch {}{}{}.html'.format(path, os.sep, name))
     if content:
-        os.system('echo {} > {}{}{}.py'.format(content, path, os.sep, name))
+        with open('{}{}{}.py'.format(path, os.sep, name), 'w'):
+            for l in content:
+                write(l)
 
 if __name__ == '__main__':
-    
+
     import path_utils
-    create_sikuli_script(os.path.join(path_utils.get_parent_dirname(getBundlePath(), 2)), "specs", "AutoHello")
+
+    new_script = os.path.join(path_utils.get_parent_dirname(getBundlePath(), 2), "specs", "AutoHello")
+    content = (
+        "# -*- coding:utf-8 -*-",
+        "",
+        "'''{}'''".format(new_script),
+        "",
+        "print('Hello Sikuli, I am scaffolded')",
+        ""
+    )
+    create_sikuli_script(new_script, content)
