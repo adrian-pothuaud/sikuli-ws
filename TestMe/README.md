6210a949768fb4eb2b80693818ae3eb31ec9c406
adrianpothuaud <adrianpothuaud2@gmail.com>
2018-02-22 06:57:49 +0100

    init TestMe App for Demos

--- TestMe/README.md
+++
@@ -1,2 +0,0 @@
-    $$ FLASK_APP=hello.py flask run
-     * Running on http://localhost:5000/
---
+++ migrate.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+"""Sikuli to Jython Workspace migration script.
+
+Migrate all in sikuli-ws into jython workspace.
+Differences between sikuli-ws and jython-ws:
+ ------------------------------------------------------------------
+| sikuli-ws                    | jython-ws                         |
+| ---------------------------- | --------------------------------- |
+| scripts in .sikuli folder    | scripts directly available as .py |
+| run scripts with sikulix.jar | run via jython interpreter        |
+ ------------------------------------------------------------------
+"""
